20015578f5af571633f0433d9c6598f1e4ed674f
Maruti Nandan Tiwari <marutint10@gmail.com>
2022-04-25 21:32:57 +0530

    First update
    Nil

---
+++ src/contracts/EthSwap.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.0;
+
+import "./Token.sol";
+
+contract EthSwap {
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint256 public rate = 100;
+
+    event TokensPurchased(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    event TokensSold(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        uint256 tokenAmount = msg.value * rate;
+
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        token.transfer(msg.sender, tokenAmount);
+
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint256 _amount) public {
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        uint256 etherAmount = _amount / rate;
+
+        require(address(this).balance >= etherAmount);
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        emit TokensSold(msg.sender, address(token), _amount, rate);
+    }
+}
